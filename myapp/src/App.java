eeb1f5957130059790dec90df817cd2500c5304f
pratheebapc <pratheebaravindran@gmail.com>
2023-02-19 19:50:13 +0530

    firstcommit

---
+++ myapp/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number=13;  
+    //Check if the number is divisible by 2 or not  
+    if(number%2==0){  
+        System.out.println("even number");  
+    }
+    else{  
+        System.out.println("odd number");  
+    }  
+}  
+    
+}
